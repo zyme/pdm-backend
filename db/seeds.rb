05a10c724fec608b2ee806696e3905be7c7e0768
Dylan Hall <dehall@mitre.org>
2018-06-19 13:44:23 -0400

    setup instructions, plus a fix to ensure rake:db:setup works

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.new(
-  email: 'test@test.com',
-  password: 'testtest',
-  password_confirmation: 'testtest'
-)
-user.skip_confirmation!
-user.save!
-
-Profile.new(name: 'Test', first_name: 'Test', last_name: 'Test')
