cbcfe777e2fc760c9874d2ef1f848deb5cd3fc00
Rob Dingwell <rob@robdingwell.org>
2018-06-20 17:13:13 -0400

    rubocop issues

--- db/migrate/20180620202351_rename_allergies_to_allergy_intolerances.rb
+++ db/migrate/20180620202351_rename_allergies_to_allergy_intolerances.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameAllergiesToAllergyIntolerances < ActiveRecord::Migration[5.2]
   def change
     rename_table :allergies, :allergy_intolerances
--- test/models/curated_model_test.rb
+++ test/models/curated_model_test.rb
@@ -15,24 +15,23 @@ class CuratedModelTest < ActiveSupport::TestCase
         snake = type.name.underscore
         create_new_success(type, p, read_test_file("#{snake.pluralize}/#{snake}_good.json"))
       rescue StandardError
-        assert false, "Error testing #{type} #{$!}"
+        assert false, "Error testing #{type} #{$ERROR_INFO}"
       end
     end
 
     p.reload
 
-    assert_equal p.allergy_intolerances.length , 1
-    assert_equal p.care_plans.length , 1
-    assert_equal p.conditions.length , 1
-    assert_equal p.devices.length , 1
-    assert_equal p.encounters.length , 1
-    assert_equal p.immunizations.length , 1
-    assert_equal p.medication_administrations.length , 1
-    assert_equal p.medication_requests.length , 1
-    assert_equal p.medication_statements.length , 1
-    assert_equal p.observations.length , 1
+    assert_equal p.allergy_intolerances.length, 1
+    assert_equal p.care_plans.length, 1
+    assert_equal p.conditions.length, 1
+    assert_equal p.devices.length, 1
+    assert_equal p.encounters.length, 1
+    assert_equal p.immunizations.length, 1
+    assert_equal p.medication_administrations.length, 1
+    assert_equal p.medication_requests.length, 1
+    assert_equal p.medication_statements.length, 1
+    assert_equal p.observations.length, 1
   end
-
 
   test 'Should validate resource for wrong type' do
     p = profiles(:harrys_profile)
@@ -43,35 +42,34 @@ class CuratedModelTest < ActiveSupport::TestCase
       begin
         puts "Testing resourceType #{type}"
 
-        mod = type.new(profile: p, resource: read_test_file("bundles/search-set.json"))
-        assert_equal false, mod.valid? , "Should not be valid with wrond resource type"
+        mod = type.new(profile: p, resource: read_test_file('bundles/search-set.json'))
+        assert_equal false, mod.valid?, 'Should not be valid with wrond resource type'
         errors = mod.errors
-        assert_equal errors["resource"], ["Wrong resource type: expected #{type.name} was Bundle"]
+        assert_equal errors['resource'], ["Wrong resource type: expected #{type.name} was Bundle"]
       rescue StandardError
-        assert false, "Error testing #{type} #{$!}"
+        assert false, "Error testing #{type} #{$ERROR_INFO}"
       end
     end
   end
 
-    test 'Should validate resource based on fhir structure' do
-      p = profiles(:harrys_profile)
-      # Device
-      types = [AllergyIntolerance, CarePlan, Condition, Encounter, Goal,
-               Immunization, MedicationAdministration, MedicationRequest, MedicationStatement,
-               Observation, Procedure]
-      types.each do |type|
-        begin
-          puts "Testing invalid resource for  #{type}"
-          snake = type.name.underscore
-          mod = type.new(profile: p, resource: read_test_file("#{snake.pluralize}/#{snake}_bad.json"))
-          mod.valid?
-          errors = mod.errors
-          assert_equal false, mod.valid? , "Should not be valid with invalid fhir resource"
-          assert errors["resource_errors"].length > 0, "Resource validation errors should be greater than 0 for #{type}"
-        rescue StandardError
-          assert false, "Error testing #{type} #{$!}"
-        end
+  test 'Should validate resource based on fhir structure' do
+    p = profiles(:harrys_profile)
+    # Device
+    types = [AllergyIntolerance, CarePlan, Condition, Encounter, Goal,
+             Immunization, MedicationAdministration, MedicationRequest, MedicationStatement,
+             Observation, Procedure]
+    types.each do |type|
+      begin
+        puts "Testing invalid resource for  #{type}"
+        snake = type.name.underscore
+        mod = type.new(profile: p, resource: read_test_file("#{snake.pluralize}/#{snake}_bad.json"))
+        mod.valid?
+        errors = mod.errors
+        assert_equal false, mod.valid?, 'Should not be valid with invalid fhir resource'
+        assert_not errors['resource_errors'].empty?, "Resource validation errors should be greater than 0 for #{type}"
+      rescue StandardError
+        assert false, "Error testing #{type} #{$ERROR_INFO}"
       end
     end
-
+  end
 end
