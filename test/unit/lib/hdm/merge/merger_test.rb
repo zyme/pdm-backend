aa4c1ddfda480c23a91b00aeb7e4e81b0ae458ef
Rob Dingwell <rob@robdingwell.org>
2018-06-21 18:01:09 -0400

    adding test for previously merged records

--- test/unit/lib/hdm/merge/merger_test.rb
+++ test/unit/lib/hdm/merge/merger_test.rb
@@ -63,6 +63,32 @@ class HDM::Merge::MergerTest < ActiveSupport::TestCase
     assert_equal profile.observations.length, 1
   end
 
+  test 'should not merge records that have already been merged' do
+    profile = profiles(:harrys_profile)
+    provider = profile.providers.first
+    load_resources(profile, provider)
+    assert_equal 12, profile.resources.where(merged: false).count
+    merger = HDM::Merge::Merger.new
+    # called multiple times to simulate updating the profile more than once 
+    merger.update_profile(profile)
+    merger.update_profile(profile)
+    merger.update_profile(profile)
+    profile.reload
+    assert_equal 0, profile.resources.where(merged: false).count
+    assert_equal 12, profile.resources.where(merged: true).count
+
+    assert_equal profile.allergy_intolerances.length, 1
+    assert_equal profile.care_plans.length, 1
+    assert_equal profile.conditions.length, 1
+    assert_equal profile.devices.length, 1
+    assert_equal profile.encounters.length, 1
+    assert_equal profile.immunizations.length, 1
+    assert_equal profile.medication_administrations.length, 1
+    assert_equal profile.medication_requests.length, 1
+    assert_equal profile.medication_statements.length, 1
+    assert_equal profile.observations.length, 1
+  end
+
   private
 
   def load_resources(profile, provider)
