127c1edbb57971a8694f8654be9aceb334581caa
achakraborty <achakraborty@mitre.org>
2018-07-05 09:06:36 -0400

    Added audit log fuctionality
    Cleaned up some lines of code
    
    Modified the schema file
    
    Modified the schema file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,27 @@ class ApplicationController < ActionController::API
   end
 
   before_action :doorkeeper_authorize!
+  before_action :update_log
 
   private
 
   # Doorkeeper methods
   def current_resource_owner
     User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
+  # Function to update the audit log as new events occur in a user's account.
+  def update_log
+    current_user = current_resource_owner
+    if current_user.nil?
+      current_user = "N/A"
+    else
+      current_user = current_user.id
+    end
+    description = controller_name + " " + action_name
+    params.each do |variable|
+      description = description + " " + variable.to_s
+    end
+    new_audit_log = AuditLog.create(requester_info: current_user, event: "event", event_time: (Time.new).inspect, description:description)
   end
 end
