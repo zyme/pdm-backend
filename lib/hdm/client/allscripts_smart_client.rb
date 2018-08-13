75b0f2f90b4ad13fb59ef76ffc413b30ecc4305d
Rob Dingwell <rob@robdingwell.org>
2018-08-13 13:53:37 -0400

    removing code that is no longer needed as it is handled by the generic smart client

--- lib/hdm/client/allscripts_smart_client.rb
+++ lib/hdm/client/allscripts_smart_client.rb
@@ -3,14 +3,6 @@
 module HDM
   module Client
     class AllscriptsSmartClient < SmartClient
-      def get_access_token(code, params = {})
-        options = get_endpoint_params
-        options[:site] = provider.base_endpoint
-        options[:raise_errors] = true
-        params[:redirect_uri] = 'http://localhost:3001/oauth'
-        client = OAuth2::Client.new(provider.client_id, provider.client_secret, options)
-        client.auth_code.get_token(code, params)
-      end
 
       def fhir_search_client(profile_provider)
         FHIR::Client.new("#{provider.base_endpoint}/Patient/#{profile_provider.subject_id}")
@@ -18,9 +10,7 @@ module HDM
 
       def fhir_search_params(profile_provider)
         params = {}
-        # see https://www.hl7.org/fhir/search.html#lastUpdate
         params[:_lastUpdated] = 'gt' + profile_provider.last_sync.iso8601 if profile_provider.last_sync
-
         { parameters: params }
       end
 
