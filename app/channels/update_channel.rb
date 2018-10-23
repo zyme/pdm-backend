71a95424f69ed84450bc7d4da1fa95407a7d73f4
Rob Dingwell <rob@robdingwell.org>
2018-10-23 15:09:13 -0400

    :shrug:

--- app/channels/update_channel.rb
+++ app/channels/update_channel.rb
@@ -4,5 +4,6 @@ class UpdateChannel < ApplicationCable::Channel
   def subscribed
     current_profile = current_user.profiles.find(params[:profile_id])
     stream_for current_profile
+    UpdateChannel.broadcast_to(current_profile, current_profile.reload.bundle_everything)
   end
 end
