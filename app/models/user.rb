aa67bbf5a19a3a82ebe52397a463e6324b25b308
lelevine <lelevine@mitre.org>
2019-09-04 11:46:53 -0400

    removed comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,13 +15,4 @@ class User < ApplicationRecord
                 last_name: last_name,
                 user_id: id).save
   end
-
-  #validate :password_complexity
-  
-  #def password_complexity
-    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
-    #return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
-    #
-    #errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
-  #end
 end
