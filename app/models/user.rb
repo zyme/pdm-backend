ac9123bf35aa64975f41b906e86f9a09e1398645
lelevine <lelevine@mitre.org>
2019-09-04 11:43:55 -0400

    added devise security extension for validating password compleity

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :secure_validatable
 
   has_many :profiles
   validates :first_name, :last_name, presence: true
@@ -15,4 +15,13 @@ class User < ApplicationRecord
                 last_name: last_name,
                 user_id: id).save
   end
+
+  #validate :password_complexity
+  
+  #def password_complexity
+    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    #return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+    #
+    #errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  #end
 end
